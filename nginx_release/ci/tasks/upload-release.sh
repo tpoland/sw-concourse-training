104f096ccba51b10dca63b1cd5ebb0595e2895e3
Tyler Poland <tyler.poland@gmail.com>
2018-11-07 21:54:44 +0000

    updating for lab 2

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -5,15 +5,16 @@ set -x
 #The director is expected to be secured and only locally available for this lab session
 #But this does not demonostrate a best practice
 
-export BOSH_DEPLOYMENT=<replace-me>-nginx
-export BOSH_DIRECTOR=https://10.200.192.0:25555/
-export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=<replace-me>
-export BOSH_ENVIRONMENT=training-bosh
+export BOSH_DEPLOYMENT=${GITHUB_USERNAME}-nginx
+export BOSH_DIRECTOR=$(bosh int creds.yml --path /bosh_url)
+# export BOSH_CLIENT=admin
+# export BOSH_CLIENT_SECRET=<replace-me>
+export BOSH_ENVIRONMENT=training
+export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/dg_director_ca.pem
 
 cd source-code/nginx_release
 
-curl -LO https://unreal-snw.s3.amazonaws.com/training-bosh.pem
+curl -LO ${CA_CERT_URL}
 bosh alias-env --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
 
 bosh login
