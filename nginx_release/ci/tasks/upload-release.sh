791ed87294f1ed944e3ead764403af2b80845683
Tyler Poland <tyler.poland@gmail.com>
2018-11-07 22:35:44 +0000

    fixup upload

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -5,10 +5,10 @@ set -x
 #The director is expected to be secured and only locally available for this lab session
 #But this does not demonostrate a best practice
 
-export BOSH_DEPLOYMENT=${GITHUB_USERNAME}-nginx
+export BOSH_DEPLOYMENT=tpoland-nginx
 export BOSH_DIRECTOR='https://10.4.1.4:25555'
-# export BOSH_CLIENT=admin
-# export BOSH_CLIENT_SECRET=<replace-me>
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT='training'
 export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
 
