7659eaea329d3373b9f57f699ecf68958b8e7f04
Tyler Poland <tyler.poland@gmail.com>
2018-11-08 16:04:03 +0000

    fixing deploy-release script

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-export BOSH_DEPLOYMENT=<replace-me>-nginx
-export BOSH_DIRECTOR=https://10.200.192.0:25555/
+export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
+export BOSH_DEPLOYMENT=tpoland-nginx
+export BOSH_DIRECTOR=https://10.4.1.4:25555/
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=<replace-me>
-export BOSH_ENVIRONMENT=training-bosh
+export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
+export BOSH_ENVIRONMENT=training
 
 cd source-code/nginx_release
 
-curl -LO https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-bosh alias-env --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
+curl -LO ${CA_CERT_URL}
+bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR}
 bosh login
 
 bosh deploy --non-interactive manifests/manifest.yml
