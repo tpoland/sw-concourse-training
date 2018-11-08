9bd4395b8591633349a2c15d102d641eb28a9245
Tyler Poland <tyler.poland@gmail.com>
2018-11-08 22:45:19 +0000

    update lab5

--- nginx_release/ci/tasks/upload-release-supplied-cert.sh
+++ nginx_release/ci/tasks/upload-release-supplied-cert.sh
@@ -11,6 +11,9 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT='training'
 
+ls ./
+ls /tmp/build/get
+
 bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert /tmp/build/get/training-bosh.pem -e ${BOSH_DIRECTOR}
 bosh login
 
