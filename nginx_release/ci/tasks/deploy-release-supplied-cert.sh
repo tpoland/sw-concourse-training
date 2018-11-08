a462c89cd00ebd983cd2a19afc45deaa7d3ffd9f
Tyler Poland <tyler.poland@gmail.com>
2018-11-08 22:34:18 +0000

    updated release task

--- nginx_release/ci/tasks/deploy-release-supplied-cert.sh
+++ nginx_release/ci/tasks/deploy-release-supplied-cert.sh
@@ -6,9 +6,8 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT=training
 
-cd source-code/nginx_release
-
-bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert ~/training-bosh.pem -e ${BOSH_DIRECTOR}
+bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR}
 bosh login
 
+cd source-code/nginx-release
 bosh deploy --non-interactive manifests/manifest.yml
--- nginx_release/ci/tasks/upload-release-supplied-cert.sh
+++ nginx_release/ci/tasks/upload-release-supplied-cert.sh
@@ -11,10 +11,8 @@ export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT='training'
 
-cd source-code/nginx_release
-
-bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert ~/training-bosh.pem -e ${BOSH_DIRECTOR}
-
+bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR}
 bosh login
 
+cd source-code/nginx_release
 bosh upload-release releases/release.gz
