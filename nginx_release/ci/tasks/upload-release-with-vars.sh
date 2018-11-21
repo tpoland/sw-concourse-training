e26cb8e21480eedfa8675c9c0605687910ca5b97
Tyler Poland <tyler.poland@gmail.com>
2018-11-21 03:31:17 +0000

    updating lab 8

--- nginx_release/ci/tasks/upload-release-with-vars.sh
+++ nginx_release/ci/tasks/upload-release-with-vars.sh
@@ -4,7 +4,7 @@
 #This is on purpose and will be covered in the security tutorial.
 #The director is expected to be secured and only locally available for this lab session
 #But this does not demonostrate a best practice
-vault login
+vault login 966f2023-2835-ccec-b543-030071abb959
 vault read -tls-skip-verify secret/training/concourse/tpoland/tpoland-pipeline
 
 set -x
